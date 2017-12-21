081f38ba2b030512e5e983c49a7256a15e5e9b0d
nadaveshed <nadav3200@gmail.com>
2017-12-21 12:48:14 +0200

    git upload

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -4,7 +4,7 @@ import { MessagesComponent } from "./messages/messages.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 
 const APP_ROUTES: Routes = [
-    { path: '', redirectTo: '/messages', pathMatch: 'full' },
+    { path: '', redirectTo: '/auth/signin', pathMatch: 'full' },
     { path: 'messages', component: MessagesComponent },
     { path: 'auth', component: AuthenticationComponent, loadChildren: './auth/auth.module#AuthModule' }
 ];
