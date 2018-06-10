219ed630d59b1586ddce1619305e3a7f44ae8254
nadaveshed <nadav3200@gmail.com>
2018-06-10 22:50:48 +0300

    fixed dropdown menu

--- assets/app/auth/authentication.component.ts
+++ assets/app/auth/authentication.component.ts
@@ -4,12 +4,12 @@ import { AuthService } from "./auth.service";
 @Component({
     selector: 'app-authentication',
     template: `
-        <header class="row spacing">
+        <header class="row spacing" style = "margin-top: 5%">
             <nav class="col-md-8 col-md-offset-2">
                 <ul class="nav nav-tabs">
                     <li routerLinkActive="active"><a [routerLink]="['signup']">הרשמה</a></li>
                     <li routerLinkActive="active" *ngIf="!isLoggedIn()"><a [routerLink]="['signin']">התחברות</a></li>
-                    <li routerLinkActive="active" *ngIf="isLoggedIn()"><a [routerLink]="['logout']">התנתק</a></li>
+                    <li routerLinkActive="active" *ngIf="isLoggedIn()"><a [routerLink]="['logout']">התנתקות</a></li>
                 </ul>
             </nav>
         </header>
