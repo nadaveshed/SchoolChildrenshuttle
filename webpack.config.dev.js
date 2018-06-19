bdafaaf7f7a2ffb9195e738423fce68f11cf3f18
nadaveshed <nadav3200@gmail.com>
2018-06-19 21:50:31 +0300

    heruko 6s

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,7 +8,7 @@ module.exports = webpackMerge(commonConfig, {
 
     output: {
         path: path.resolve(__dirname + '/public/js/app'),
-        publicPath: "/",
+        publicPath: "/js/app/",
         filename: 'bundle.js',
         chunkFilename: '[id].chunk.js'
     },
